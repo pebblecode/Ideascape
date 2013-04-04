c251427344c78f5457bdbfaa77927c79f944ab67
Simon Dickson <simon.dickson@pebblecode.com>
2013-04-04 16:01:59 +0100

    Intial trending implementation

---
+++ Ideascape/Models/Trending.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Ideascape.Data.Entities;
+
+namespace Ideascape.Models
+{
+    public class Trending
+    {
+        public Idea TrendingIdea { get; set; }
+
+        public IEnumerable<string> TrendingTags { get; set; }  
+    }
+}
