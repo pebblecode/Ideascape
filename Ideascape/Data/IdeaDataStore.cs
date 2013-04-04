e3c6e946daa8a31ef412b9d23129a521de7cb619
Nathan Evans <nathan.evans@pebblecode.com>
2013-04-04 13:40:53 +0100

    Stuff.

--- Ideascape/Data/IdeaDataStore.cs
+++ Ideascape/Data/IdeaDataStore.cs
@@ -15,11 +15,10 @@ namespace Ideascape.Data
         {
             try
             {
-                using (
-                    var fs = new FileStream("ideas.json", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+                using (var fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "ideas.json"), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
                 using (var sr = new StreamReader(fs))
                 using (var jr = new JsonTextReader(sr))
-                    Items = new JsonSerializer().Deserialize<List<Idea>>(jr);
+                    Items = new JsonSerializer().Deserialize<List<Idea>>(jr) ?? new List<Idea>();
             }
             catch
             {
@@ -29,7 +28,7 @@ namespace Ideascape.Data
 
         public void Save()
         {
-            using (var fs = new FileStream("ideas.json", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+            using (var fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "ideas.json"), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
             using (var sw = new StreamWriter(fs))
                 new JsonSerializer().Serialize(sw, Items);
         }
---
+++ Ideascape/Infrastructure/Class1.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ideascape.Infrastructure
+{
+    public static class Class1
+    {
+        public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index/3)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+    }
+}
